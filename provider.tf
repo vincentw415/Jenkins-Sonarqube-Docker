dac3cc029f98be95087a55b5bf4fd90a76cab518
vincentw415 <125775007+vincentw415@users.noreply.github.com>
2023-07-25 19:37:11 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
-provider "azure"{
-  region = "West-US-2"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
 }
