ad8a2a83ed69e259ddb948b7af80f64954f90e6b
vincentw415 <125775007+vincentw415@users.noreply.github.com>
2023-07-25 19:39:06 -0700

    Update test-deploy-VNet.tf

--- test-deploy-VNet.tf
+++ test-deploy-VNet.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
 resource "azure_test_VNet" "example"{
   name                      = "example-vnet"
   location                  = us-west-2
