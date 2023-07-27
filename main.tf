4c4cf40bf0b460d8c9be5ab8e76df8f7874f05d7
vincentw415 <125775007+vincentw415@users.noreply.github.com>
2023-07-26 18:40:43 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
   name                = "test-vnet"
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
-  address_space       = "10.0.0.0/16"
+  address_space       = ["10.0.0.0/16"]
 }
 
 # Create a subnet within the VNET
