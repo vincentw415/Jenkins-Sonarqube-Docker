e72729fef931823ab8a7628d727d0df45a832312
vincentw415 <125775007+vincentw415@users.noreply.github.com>
2023-07-26 22:47:38 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,3 @@
-variable "client_secret" {
-  description = "secret for using the jenkins service principal"
-  type        = string
-  sensitive   = true
-}
-
-variable "subscription_id" {
-  description = "subscription id for the subscription jenkins is deployed on"
-  type        = string
-}
-
-variable "client_id" {
-  description = "client/application id of the jenkins service principal"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "tenant of landing zone"
-  type        = string
-}
-
 variable "region" {
   description = "Azure region"
   default     = "westus2"
