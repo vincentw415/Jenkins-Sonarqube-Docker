500ab36a3b90d3aa4a9c83bee46ed40bf421d9e4
vincentw415 <125775007+vincentw415@users.noreply.github.com>
2023-07-26 21:56:59 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,23 @@ variable "region" {
   description = "Azure region"
   default     = "westus2"
 }
+
+variable "region" {
+  description = "Azure region"
+  default     = "westus2"
+}
+
+variable "region" {
+  description = "Azure region"
+  default     = "westus2"
+}
+
+variable "region" {
+  description = "Azure region"
+  default     = "westus2"
+}
+
+variable "region" {
+  description = "Azure region"
+  default     = "westus2"
+}
