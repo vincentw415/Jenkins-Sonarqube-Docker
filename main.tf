3c42bc10c2f6fe64e610f85e5206368d1b8cde42
vincentw415 <125775007+vincentw415@users.noreply.github.com>
2023-07-26 18:45:09 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Create a resource group
 resource "azurerm_resource_group" "test" {
   name     = "test-terraform-deployment"
-  location = "var.region"
+  location = var.region
 }
 
 # Create a virtual network within the resource group
