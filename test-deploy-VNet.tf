fb1ab138fb357c8e19f844477e8f4d1b79624fc1
vincentw415 <125775007+vincentw415@users.noreply.github.com>
2023-07-26 17:54:50 -0700

    Update test-deploy-VNet.tf

--- test-deploy-VNet.tf
+++ test-deploy-VNet.tf
@@ -11,6 +11,11 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  subscription_id = "19397021-31de-4c49-ab60-493189887dac"
+  client_id       = "020554a0-70ee-4b8d-b4bf-6b446878ef97"
+  client_secret   = "UZ-8Q~G6FMrNJ5QRnLEzsJfOTWkvOF74exg7dbcA"
+  tenant_id       = "d20b3607-44f7-4082-b310-f13e8927692b"
 }
 
 # Create a resource group
