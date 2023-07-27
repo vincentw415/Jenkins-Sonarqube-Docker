30695dcf6391e2bc6d88a626aea983a0db3ec991
vincentw415 <125775007+vincentw415@users.noreply.github.com>
2023-07-26 22:47:52 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,8 +11,4 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
