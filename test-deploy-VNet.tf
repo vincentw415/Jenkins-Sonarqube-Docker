715edbbfcfaf99d873ec33b4bedf16ee031774d5
vincentw415 <125775007+vincentw415@users.noreply.github.com>
2023-07-26 18:02:04 -0700

    Update test-deploy-VNet.tf

--- test-deploy-VNet.tf
+++ test-deploy-VNet.tf
@@ -22,7 +22,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "test" {
   name     = "test-VNet"
   location = "West US 2"
-  tags     = "updated"
+  tag      = "updated"
 }
 
 # Create a virtual network within the resource group
