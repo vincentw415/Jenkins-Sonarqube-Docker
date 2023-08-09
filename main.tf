e6078a27125f32fa94a67aa6cf7f58734496d4df
Vincent Wong <you@example.com>
2023-08-09 15:52:25 -0700

    udpated network name

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "azurerm_virtual_network" "network" {
 resource "azurerm_subnet" "subnet" {
   name                  = "test-subnet"
   resource_group_name   = azurerm_resource_group.resource_group.name
-  virtual_network_name  = "test-vnet"
+  virtual_network_name  = azurerm_resource_group.network.name
   address_prefixes      = ["10.0.0.0/16"]
 }
 
