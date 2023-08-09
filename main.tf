d12f3e7f60e3fe2ead56d0013e1d3ffa68a27847
Vincent Wong <you@example.com>
2023-08-09 16:08:39 -0700

    updated vm

--- main.tf
+++ main.tf
@@ -39,20 +39,30 @@ resource "azurerm_virtual_machine" "vm" {
   location              = azurerm_resource_group.resource_group.location
   resource_group_name   = azurerm_resource_group.resource_group.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  size                  = "Standard_DS1_v2"
-  admin_username        = "adminuser"
-  admin_password        = "password123!"
+  vm_size                  = "Standard_DS1_v2"
 
-  source_image_reference {
+  storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "20.04-LTS"
     version   = "latest"
   }
-
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
   }
 }
 
