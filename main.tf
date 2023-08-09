f21964c2a3648dd0701aec17fc928c045b5ba598
Vincent Wong <you@example.com>
2023-08-09 16:21:01 -0700

    updated image references

--- main.tf
+++ main.tf
@@ -39,12 +39,12 @@ resource "azurerm_virtual_machine" "vm" {
   location              = azurerm_resource_group.resource_group.location
   resource_group_name   = azurerm_resource_group.resource_group.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size                  = "Standard_DS1_v2"
+  vm_size               = "Standard_DS1_v2"
 
   storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-datacenter"
     version   = "latest"
   }
   storage_os_disk {
