4195523ebf0d91dd6f84c30200e6d3a90a45960c
Vincent Wong <you@example.com>
2023-08-09 15:58:08 -0700

    fixed vnet name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.region
 }
 
-# Create a tes virtual network
+# Create a test virtual network
 resource "azurerm_virtual_network" "network" {
   name                = "test-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -16,7 +16,7 @@ resource "azurerm_virtual_network" "network" {
 resource "azurerm_subnet" "subnet" {
   name                  = "test-subnet"
   resource_group_name   = azurerm_resource_group.resource_group.name
-  virtual_network_name  = azurerm_resource_group.network.name
+  virtual_network_name  = azurerm_virtual_network.network.name
   address_prefixes      = ["10.0.0.0/16"]
 }
 
